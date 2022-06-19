08d5e629cce446740f2bb8a785f76e8b5b2a0fd0
Sam5196 <sammedpachore1008@gmail.com>
2022-06-19 20:38:38 +0530

    Added:maximum number of float

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -4,9 +4,20 @@ public class MaximumNumber {
     public static void main(String[] args) {
         System.out.println("Welcome Generics program......");
         System.out.println("Max Number of three Integer :" + Number(34, 48, 21));
+        System.out.println("Max Number of three Float :"+ Number(3.5f, 9.6f, 10.3f));
     }
     public static Integer Number(Integer a, Integer b, Integer c) {
         Integer max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
+    }
+    public static Float Number(Float a, Float b, Float c) {
+        Float max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
