491d25ac4fd389032c0337b6baad26622264caab
Sam5196 <sammedpachore1008@gmail.com>
2022-06-19 20:48:31 +0530

    Added:maximum of String

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -5,6 +5,7 @@ public class MaximumNumber {
         System.out.println("Welcome Generics program......");
         System.out.println("Max Number of three Integer :" + Number(34, 48, 21));
         System.out.println("Max Number of three Float :"+ Number(3.5f, 9.6f, 10.3f));
+        System.out.println("Max of three String :"+ Alphabet("Apple", "Peach" ,"Banana"));
     }
     public static Integer Number(Integer a, Integer b, Integer c) {
         Integer max = a;
@@ -18,6 +19,16 @@ public class MaximumNumber {
     }
     public static Float Number(Float a, Float b, Float c) {
         Float max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
+    }
+    public static String Alphabet(String a, String b, String c) {
+        String max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
