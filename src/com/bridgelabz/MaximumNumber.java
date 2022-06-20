6db3e4d024572300d70ab5115f669edd36c84ba6
Sam5196 <sammedpachore1008@gmail.com>
2022-06-20 16:11:06 +0530

    Added:refactor all three to one generic method

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -5,10 +5,10 @@ public class MaximumNumber {
         System.out.println("Welcome Generics program......");
         System.out.println("Max Number of three Integer :" + Number(34, 48, 21));
         System.out.println("Max Number of three Float :"+ Number(3.5f, 9.6f, 10.3f));
-        System.out.println("Max of three String :"+ Alphabet("Apple", "Peach" ,"Banana"));
+        System.out.println("Max of three String :"+ Number("Apple", "Peach" ,"Banana"));
     }
-    public static Integer Number(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    public static <T extends Comparable> T Number(T a, T b, T c) {
+        T max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
@@ -17,28 +17,6 @@ public class MaximumNumber {
         }
         return max;
     }
-    public static Float Number(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-    public static String Alphabet(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-
-
 }
 
 
